2233459cadbabfa5d92a07e8bff2f1c6d883a2ba
Matheus Souza <matheushrt@gmail.com>
2020-03-13 11:12:42 -0300

    refactored

--- src/middlewares/webhookMiddleware.ts
+++ src/middlewares/webhookMiddleware.ts
@@ -1,17 +1,21 @@
 import { RequestHandler } from 'express';
 import { Message } from 'node-telegram-bot-api';
-import * as botCommands from '../bot-commands';
+import * as botCommands from '../bot/commands';
 
 export const webhookMiddleware: RequestHandler = async (req, res) => {
   const { body } = req;
   const message: Message = body?.message || null;
-  const { text } = message;
-  const command = text.includes('@')
-    ? text.split('@')[0].substr(1)
-    : text.substr(1);
-
-  const botCommand = `${command}Command`;
-  botCommands[botCommand](message);
+  const isBotCommand = message?.entities?.find(e => e.type === 'bot_command');
+  if (isBotCommand) {
+    const { text } = message;
+    const command = text?.includes('@')
+      ? text.split('@')[0].substr(1)
+      : text.substr(1);
+    const botCommand = `${command}Command`;
+    botCommands[botCommand](message);
+  } else {
+    console.warn('Not a bot command:', body);
+  }
 
   res.sendStatus(200);
 };
